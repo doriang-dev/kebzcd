54d62bb9f73be71e0226076f14d674bba3df409d
doriang-dev <69525163+doriang-dev@users.noreply.github.com>
2020-12-21 15:00:01 +0100

    First version without second part (@0.1.0)

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      title: ['Lato'],
+      body: ['Playfair Display']
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
